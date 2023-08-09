7adb03195a3816736f34f3aa514773ff35985c42
Matteo Esposito Marroccella <93797390+matteoespo@users.noreply.github.com>
2023-08-09 18:25:55 +0200

    social

--- components/Home/SocialMediaArround/SocialMediaArround.tsx
+++ components/Home/SocialMediaArround/SocialMediaArround.tsx
@@ -32,13 +32,11 @@ export default function SocialMediaEmail(props: { finishedLoading: boolean }) {
         <div className="flex flex-col space-y-8 justify-center items-center">
           <div className="flex flex-col justify-center items-center space-y-5">
             {/* Github Icon */}
-            <IconClickableWithAnimation Icon={GithubIcon} href={"https://github.com/hktitof"} />
+            <IconClickableWithAnimation Icon={GithubIcon} href={"https://github.com/matteoespo"} />
             {/* Linkedin icon */}
-            <IconClickableWithAnimation Icon={LinkedinIcon} href={"https://www.linkedin.com/in/abdellatif-anaflous/"} />
+            <IconClickableWithAnimation Icon={LinkedinIcon} href={"https://www.linkedin.com/in/matteo-esposito-marroccella/"} />
             {/* Instagram Icon */}
-            <IconClickableWithAnimation Icon={InstagramIcon} href={"https://www.instagram.com/xredme/"} />
-            {/* Youtube Icon */}
-            <IconClickableWithAnimation Icon={YoutubeIcon} href={"https://www.youtube.com/@abdellatif_anaflous"} />
+            <IconClickableWithAnimation Icon={InstagramIcon} href={"https://www.instagram.com/_matteo.espo_/"} />
           </div>
           <div className="h-28 w-0.5 bg-gray-400"></div>
         </div>
