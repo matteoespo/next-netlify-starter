199552ae6f76e850685ab6d51b10549f0c0c8fd6
Matteo Esposito Marroccella <93797390+matteoespo@users.noreply.github.com>
2023-08-12 23:39:18 +0200

    ef

--- pages/userdatapuller/index.tsx
+++ pages/userdatapuller/index.tsx
@@ -302,7 +302,7 @@ export default function Page() {
         {/* // ? About */}
         <About />
       </div>
-      <Footer githubUrl="https://github.com/hktitof/DataPuller" hideSocialsInDesktop={false} />
+      <Footer Url="https://github.com/matteoespo" hideSocialsInDesktop={false} />
     </>
   );
 }
