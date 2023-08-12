a6f3bef27c48650e808a05c0adb898c9dadcb6fb
Matteo Esposito Marroccella <93797390+matteoespo@users.noreply.github.com>
2023-08-12 22:44:50 +0200

    e

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import MyName from "../components/Home/MyName/MyName";
 import { useContext, useEffect, useState, useRef } from "react";
 import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
 import AboutMe from "../components/Home/AboutMe/AboutMe";
+import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
 import WhereIHaveWorked from "../components/Home/WhereIHaveWorked/WhereIHaveWorked";
 import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIveBuilt";
 import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
@@ -14,10 +15,40 @@ import "aos/dist/aos.css";
 import Head from "next/head";
 import ScreenSizeDetector from "../components/CustomComponents/ScreenSizeDetector";
 export default function Home() {
+  const [ShowElement, setShowElement] = useState(false);
+  const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
+  const [ShowMe, setShowMe] = useState(false);
   // context Variable to clearInterval
   const context = useContext(AppContext);
   const aboutRef = useRef<HTMLDivElement>(null);
   const homeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // remove the interval Cookie timer setter when
+    clearInterval(context.sharedState.userdata.timerCookieRef.current);
+    if (typeof window !== "undefined") {
+      // remove UserDataPuller project EventListeners
+      window.removeEventListener("resize", context.sharedState.userdata.windowSizeTracker.current);
+      window.removeEventListener("mousemove", context.sharedState.userdata.mousePositionTracker.current, false);
+      // remove Typing project EventListeners
+      window.removeEventListener("resize", context.sharedState.typing.eventInputLostFocus);
+      document.removeEventListener("keydown", context.sharedState.typing.keyboardEvent);
+    }
+    setTimeout(() => {
+      setShowElement(true);
+    }, 4500);
+
+    setTimeout(() => {
+      setShowThisCantBeReached(false);
+    }, 5400);
+    // ? INFORMATIONAL next function will show the component after changing the state of ShowMe
+    setTimeout(() => {
+      setShowElement(false);
+      setShowMe(true);
+      context.sharedState.finishedLoading = true;
+      context.setSharedState(context.sharedState);
+    }, 10400);
+  }, [context, context.sharedState]);
 
   useEffect(() => {
     Aos.init({ duration: 2000, once: true });
@@ -52,6 +83,7 @@ export default function Home() {
         <meta name="twitter:image" content={meta.image} />
       </Head>
       <div className="relative snap-mandatory min-h-screen bg-AAprimary w-full ">
+        {context.sharedState.finishedLoading ? <></> : ShowThisCantBeReached ? <ThisCantBeReached /> : <></>}
         {context.sharedState.finishedLoading ? <></> : ShowElement ? <Startup /> : <></>}
         <Header finishedLoading={context.sharedState.finishedLoading} sectionsRef={homeRef} />
         <MyName finishedLoading={context.sharedState.finishedLoading} />
