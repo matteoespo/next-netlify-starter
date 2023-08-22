97e6691dc1cdb6652391c73e4f88737a3d9f16c8
Matteo Esposito Marroccella <93797390+matteoespo@users.noreply.github.com>
2023-08-22 18:13:37 +0200

    e

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Matteo Esposito Marroccella </span>
+            from <span className="purple"> Verona, Italy, living in Munich, Germany.</span>
             <br /> I am a final year student pursuing an Integrated MSc (IMSc)
             in Maths and Computing at BIT Mesra.
             <br />
             Additionally, I am currently employed as a software developer at
-            Juspay.
+            ATS Europe.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,7 +24,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Work Out
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
